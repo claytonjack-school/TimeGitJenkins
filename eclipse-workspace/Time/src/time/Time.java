aae6967b9cc93d7fcf2d62f0706c635ac6b43cf1
Jack Clayton <jack@example.com>
2025-03-04 16:32:33 -0500

    test

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -22,7 +22,7 @@ import javax.swing.JOptionPane;
 public class Time {
 	public static void main(String[] args) {
 		int totalSeconds = getTotalSeconds("10:10:10");
-		System.out.println("Github Seconds = "+ totalSeconds);
+		System.out.println("Github Seconds! = "+ totalSeconds);
 		;
 	}
 
